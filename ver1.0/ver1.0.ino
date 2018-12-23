6c04cba7d501da5c4ef04d37eac7921239f85a28
tekinyasin02 <tekinyasin02@gmail.com>
2018-12-23 18:36:11 +0300

    çift joystick ile kontrol sağlandı.

--- ver1.0/ver1.0.ino
+++ ver1.0/ver1.0.ino
@@ -10,16 +10,26 @@
  const int servo_2 =9;
  int servo_2val ;
  Servo servo2 ;
+
+ const int servo_3 =10;
+ int servo_3val ;
+ Servo servo3 ;
+
+  const int servo_4 =11;
+ int servo_4val ;
+ Servo servo4 ;
  
 // degerler
 int x_deger = 0;
 int y_deger = 0; 
-int buton_deger = 0;
-
+int x1_deger = 0;
+int y1_deger = 0; 
 void setup() {
   Serial.begin(9600);
    servo1.attach(servo_1);
    servo2.attach(servo_2);
+   servo3.attach(servo_3);
+   servo4.attach(servo_4);
   pinMode(buton_pin,INPUT_PULLUP);
   
 }
@@ -28,21 +38,15 @@ void loop() {
 kontrol();
 servo1_kontrol(y_deger);
 servo2_kontrol(x_deger);
-
+servo3_kontrol(x1_deger);
+servo4_kontrol(y1_deger);
 }
 void kontrol()
 {
-   x_deger = analogRead(A14);
- y_deger = analogRead(A15);
- buton_deger = digitalRead(buton_pin);
-// Serial.println("X Değeri:");
-// Serial.println(x_deger);
-// Serial.println("Y Değeri:");
-// Serial.println(y_deger);
-// Serial.println("Buton Durumu");
-// Serial.println(buton_deger);
-//
-// delay(1000);
+  x_deger = analogRead(A14);
+    y_deger = analogRead(A15);
+  x1_deger = analogRead(A13);
+  y1_deger = analogRead(A12);
  
   }
 void servo1_kontrol(int data)
@@ -56,6 +60,22 @@ void servo1_kontrol(int data)
 {
   servo_2val=map(data,0,1023,0,180);
   servo2.write(servo_2val);
+    delay(10);
+  
+  }
+    void servo3_kontrol(int data)
+{
+if(data > 450)
+     {
+  servo_3val=map(data,0,1023,0,180);
+  servo3.write(servo_3val);
+    delay(10);
+     }
+  }
+    void servo4_kontrol(int data)
+{
+  servo_4val=map(data,0,1023,0,180);
+  servo4.write(servo_4val);
     delay(10);
   
   }
